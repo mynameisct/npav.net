dec4027e1b85a8cf757f4b8b96838c9572a663c8
Chaitanya <chaitanya.tribhuwan100@gmail.com>
2025-12-28 19:50:46 +0530

    Updated BuyNowPage

--- src/test/java/com/npav/net/pages/BuyNowPage.java
+++ src/test/java/com/npav/net/pages/BuyNowPage.java
@@ -1,14 +1,23 @@
 package com.npav.net.pages;
 
 import com.npav.net.page.base.BasePage;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
 
 public class BuyNowPage extends BasePage {
 
     @FindBy(xpath = "//a[@href='https://npav.net/buynow']/button")
     WebElement buyNowBtn;
+
+    @FindBy(xpath = "//ul[@class='tabs nav nav-tabs']")
+    List<WebElement> allOptions;
 
     public BuyNowPage(WebDriver driver)
     {
