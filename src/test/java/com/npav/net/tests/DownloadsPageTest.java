8ffd62eb9f3595a39b1d70eac7ce56740a41f5c5
Chaitanya <chaitanya.tribhuwan100@gmail.com>
2025-12-28 19:43:58 +0530

    Added Simple Downloads Page and Tests

---
+++ src/test/java/com/npav/net/tests/DownloadsPageTest.java
@@ -0,0 +1,22 @@
+package com.npav.net.tests;
+
+import com.npav.net.pages.DownloadsPage;
+import com.npav.net.test.base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DownloadsPageTest extends BaseTest
+{
+    private DownloadsPage downloadsPage;
+
+    @Test
+    public void  verifyClicktoDownload()
+    {
+
+        downloadsPage = new DownloadsPage(driver);
+
+        String actualTitle = downloadsPage.clickOnDownloads();
+
+        Assert.assertEquals(actualTitle.trim()," Download best antivirus for pc | Net Protector Antivirus ".trim());
+    }
+}
